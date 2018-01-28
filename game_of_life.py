e622d2d21a74accd8aca49917d02373f1f398871
Apiquet <anthony.piquet2@gmail.com>
2018-01-28 15:16:32 -0800

    board initialized

--- game_of_life.py
+++ game_of_life.py
@@ -5,6 +5,20 @@ WITDH = 600
 HEIGHT = 600
 
 def main():
+    #pygame initialized
     pygame.init()
     pygame.display.set_caption("game of life")
-    screen = pygame.display.set_mode((WITDH,HEIGHT))
+    #creation of the window
+    window = pygame.display.set_mode((WITDH,HEIGHT))
+    
+    #creation of the board
+    board= initialize_board()
+    
+    
+    
+    
+def initialize_board():
+    return [[False for x in range(NX)] for y in range(NY)]
+
+    
+    
