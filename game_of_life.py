419b54ea63e2113ece0d9a5805749b3cf6eeeb9c
Apiquet <anthony.piquet2@gmail.com>
2018-01-28 18:28:57 -0800

    max and spaceship structure

--- game_of_life.py
+++ game_of_life.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 
 import time
 
-Nbr_Cell_x= 70
-Nbr_Cell_y=70
+Nbr_Cell_x = 70
+Nbr_Cell_y = 70
 
 Cell_Size= 8
 
@@ -44,6 +44,16 @@ def main():
                     board=initialize_board() 
                 if event.key == K_g:
                     board=glider_gun() 
+                if event.key == K_m:
+                    board=max_structure() 
+                if event.key == K_b:
+                    board=spaceship() 
+                #save structure
+                if event.key == K_s:
+                    for y in range(Nbr_Cell_y):
+                        for x in range(Nbr_Cell_x):
+                            if board[y][x]:
+                                print("board[{}][{}] = True".format(y,x))                   
                 if event.key == K_UP:
                     speed+=1 
                 
@@ -81,48 +91,279 @@ def initialize_board():
 def glider_gun():
     board = [[False for x in range(Nbr_Cell_x)] for y in range(Nbr_Cell_y)]
     #coordinates come from outputs: print("board[{}][{}] = True".format(y,x))
-    #thanks to the line above just need to click on the board to create the structure,
-    #then, copy paste the outputs as I did for the following lines
-    board[60][8] = True
-    board[61][8] = True
-    board[61][9] = True
-    board[60][9] = True
-    board[61][18] = True
-    board[60][18] = True
-    board[59][18] = True
-    board[58][19] = True
-    board[62][19] = True
+    #create a structure by clicking on the board
+    #then click on the 's' key to save it to a new function
     board[57][20] = True
     board[57][21] = True
+    board[58][19] = True
+    board[58][23] = True
+    board[59][18] = True
+    board[59][24] = True
+    board[59][32] = True
+    board[60][8] = True
+    board[60][9] = True
+    board[60][18] = True
+    board[60][22] = True
+    board[60][24] = True
+    board[60][25] = True
+    board[60][30] = True
+    board[60][32] = True
+    board[61][8] = True
+    board[61][9] = True
+    board[61][18] = True
+    board[61][24] = True
+    board[61][28] = True
+    board[61][29] = True
+    board[62][19] = True
+    board[62][23] = True
+    board[62][28] = True
+    board[62][29] = True
+    board[62][42] = True
+    board[62][43] = True
     board[63][20] = True
     board[63][21] = True
-    board[58][23] = True
-    board[59][24] = True
-    board[60][24] = True
-    board[61][24] = True
-    board[62][23] = True
-    board[60][25] = True
-    board[60][22] = True
-    board[61][28] = True
-    board[61][28] = True
-    board[61][28] = True
-    board[62][28] = True
     board[63][28] = True
     board[63][29] = True
-    board[62][29] = True
-    board[61][29] = True
-    board[60][30] = True
-    board[64][30] = True
-    board[60][32] = True
-    board[59][32] = True
-    board[64][32] = True
-    board[65][32] = True
     board[63][42] = True
     board[63][43] = True
-    board[62][43] = True
-    board[62][42] = True
+    board[64][30] = True
+    board[64][32] = True
+    board[65][32] = True
     return board
-    
+
+def spaceship():
+    board = [[False for x in range(Nbr_Cell_x)] for y in range(Nbr_Cell_y)]
+    #coordinates come from outputs: print("board[{}][{}] = True".format(y,x))
+    #create a structure by clicking on the board
+    #then click on the 's' key to save it to a new function
+    board[15][5] = True
+    board[15][6] = True
+    board[15][7] = True
+    board[15][8] = True
+    board[15][9] = True
+    board[15][10] = True
+    board[16][4] = True
+    board[16][10] = True
+    board[17][10] = True
+    board[18][4] = True
+    board[18][9] = True
+    board[19][6] = True
+    board[19][7] = True
+    board[24][5] = True
+    board[24][6] = True
+    board[24][7] = True
+    board[24][8] = True
+    board[24][9] = True
+    board[25][4] = True
+    board[25][9] = True
+    board[26][9] = True
+    board[27][4] = True
+    board[27][8] = True
+    board[28][6] = True
+    board[33][5] = True
+    board[33][6] = True
+    board[33][7] = True
+    board[33][8] = True
+    board[34][4] = True
+    board[34][8] = True
+    board[35][8] = True
+    board[36][4] = True
+    board[36][7] = True
+    return board
+
+def max_structure():
+    board = [[False for x in range(Nbr_Cell_x)] for y in range(Nbr_Cell_y)]
+    #coordinates come from outputs: print("board[{}][{}] = True".format(y,x))
+    #create a structure by clicking on the board
+    #then click on the 's' key to save it to a new function
+    board[23][44] = True
+    board[24][43] = True
+    board[24][44] = True
+    board[24][45] = True
+    board[25][38] = True
+    board[25][39] = True
+    board[25][40] = True
+    board[25][45] = True
+    board[25][46] = True
+    board[26][37] = True
+    board[26][40] = True
+    board[26][41] = True
+    board[26][42] = True
+    board[26][45] = True
+    board[26][47] = True
+    board[26][48] = True
+    board[27][36] = True
+    board[27][40] = True
+    board[27][42] = True
+    board[27][45] = True
+    board[27][47] = True
+    board[28][36] = True
+    board[28][41] = True
+    board[28][43] = True
+    board[28][45] = True
+    board[28][47] = True
+    board[28][49] = True
+    board[28][50] = True
+    board[29][38] = True
+    board[29][43] = True
+    board[29][45] = True
+    board[29][49] = True
+    board[29][50] = True
+    board[30][26] = True
+    board[30][27] = True
+    board[30][28] = True
+    board[30][29] = True
+    board[30][35] = True
+    board[30][37] = True
+    board[30][42] = True
+    board[30][46] = True
+    board[30][48] = True
+    board[30][49] = True
+    board[30][50] = True
+    board[31][26] = True
+    board[31][30] = True
+    board[31][31] = True
+    board[31][33] = True
+    board[31][35] = True
+    board[31][36] = True
+    board[31][37] = True
+    board[31][39] = True
+    board[31][40] = True
+    board[31][50] = True
+    board[31][51] = True
+    board[32][26] = True
+    board[32][32] = True
+    board[32][33] = True
+    board[32][39] = True
+    board[33][27] = True
+    board[33][30] = True
+    board[33][31] = True
+    board[33][33] = True
+    board[33][36] = True
+    board[33][39] = True
+    board[33][41] = True
+    board[33][42] = True
+    board[34][33] = True
+    board[34][35] = True
+    board[34][37] = True
+    board[34][39] = True
+    board[34][41] = True
+    board[34][43] = True
+    board[34][49] = True
+    board[34][50] = True
+    board[34][51] = True
+    board[34][52] = True
+    board[35][27] = True
+    board[35][30] = True
+    board[35][31] = True
+    board[35][33] = True
+    board[35][36] = True
+    board[35][39] = True
+    board[35][42] = True
+    board[35][43] = True
+    board[35][45] = True
+    board[35][47] = True
+    board[35][48] = True
+    board[35][52] = True
+    board[36][26] = True
+    board[36][32] = True
+    board[36][33] = True
+    board[36][37] = True
+    board[36][39] = True
+    board[36][41] = True
+    board[36][45] = True
+    board[36][46] = True
+    board[36][52] = True
+    board[37][26] = True
+    board[37][30] = True
+    board[37][31] = True
+    board[37][33] = True
+    board[37][35] = True
+    board[37][36] = True
+    board[37][39] = True
+    board[37][42] = True
+    board[37][45] = True
+    board[37][47] = True
+    board[37][48] = True
+    board[37][51] = True
+    board[38][26] = True
+    board[38][27] = True
+    board[38][28] = True
+    board[38][29] = True
+    board[38][35] = True
+    board[38][37] = True
+    board[38][39] = True
+    board[38][41] = True
+    board[38][43] = True
+    board[38][45] = True
+    board[39][36] = True
+    board[39][37] = True
+    board[39][39] = True
+    board[39][42] = True
+    board[39][45] = True
+    board[39][47] = True
+    board[39][48] = True
+    board[39][51] = True
+    board[40][39] = True
+    board[40][45] = True
+    board[40][46] = True
+    board[40][52] = True
+    board[41][27] = True
+    board[41][28] = True
+    board[41][38] = True
+    board[41][39] = True
+    board[41][41] = True
+    board[41][42] = True
+    board[41][43] = True
+    board[41][45] = True
+    board[41][47] = True
+    board[41][48] = True
+    board[41][52] = True
+    board[42][28] = True
+    board[42][29] = True
+    board[42][30] = True
+    board[42][32] = True
+    board[42][36] = True
+    board[42][41] = True
+    board[42][43] = True
+    board[42][49] = True
+    board[42][50] = True
+    board[42][51] = True
+    board[42][52] = True
+    board[43][28] = True
+    board[43][29] = True
+    board[43][33] = True
+    board[43][35] = True
+    board[43][40] = True
+    board[44][28] = True
+    board[44][29] = True
+    board[44][31] = True
+    board[44][33] = True
+    board[44][35] = True
+    board[44][37] = True
+    board[44][42] = True
+    board[45][31] = True
+    board[45][33] = True
+    board[45][36] = True
+    board[45][38] = True
+    board[45][42] = True
+    board[46][30] = True
+    board[46][31] = True
+    board[46][33] = True
+    board[46][36] = True
+    board[46][37] = True
+    board[46][38] = True
+    board[46][41] = True
+    board[47][32] = True
+    board[47][33] = True
+    board[47][38] = True
+    board[47][39] = True
+    board[47][40] = True
+    board[48][33] = True
+    board[48][34] = True
+    board[48][35] = True
+    board[49][34] = True    
+    return board    
 
 def update_board(board):
     next_board= initialize_board()
