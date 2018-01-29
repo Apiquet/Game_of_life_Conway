4893120d5c4362f662d3b47d6f4a6fb5e20ffcf0
Apiquet <anthony.piquet2@gmail.com>
2018-01-28 16:11:54 -0800

    adding rules of the game of life

--- game_of_life.py
+++ game_of_life.py
@@ -6,6 +6,10 @@ Nbr_Cell_y=70
 
 Cell_Size= 8
 
+#Color cell = Blue
+Cell_alive = (68,101,182)
+Cell_dead = (0,0,0)
+
 WITDH = Nbr_Cell_x * Cell_Size
 HEIGHT = Nbr_Cell_y * Cell_Size
 
@@ -24,12 +28,39 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 quit()
-        
+        #update the board state to the next generation
+        board = update_board(board)
+        #fill the window with only dead cells 
+        window.fill(Cell_dead)
+        #draw on the window each cell alive
+        for y in range(Nbr_Cell_y):
+            for x in range(Nbr_Cell_x):
+                if board[y][x]:
+                    pygame.draw.rect(window, Cell_alive, (x * Cell_Size, y * Cell_Size, Cell_Size, Cell_Size))
+        #display the result
+        pygame.display.update()
     
     
 def initialize_board():
     return [[False for x in range(Nbr_Cell_x)] for y in range(Nbr_Cell_y)]
 
+def update_board(board):
+    next_board= initialize_board()
+    for x in range(Nbr_Cell_y):
+        for y in range(Nbr_Cell_x):
+            neighbors = find_cell_neighbors(board, y, x)
+            if neighbors == 3:
+                next_board[y][x] = True
+            elif neighbors == 2:
+                next_board[y][x] = board[y][x]
+            else:
+                pass
+        return next_board
     
+def find_cell_neighbors(board, x, y):
+    neighbors = 0
+    #TODO: find nbr of neighbors
+    return neighbors
+   
 if __name__ == "__main__":
     main()
